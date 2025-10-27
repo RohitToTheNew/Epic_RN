ec19786fea3e63e04bf54d825247ec4789fdda53
Vishal.Madheshiya <vishal.madheshiya@audioenhancement.com>
2025-10-27 15:47:37 +0530

    icon setup and change bundle id

--- EpicMobileApp/android/app/src/main/java/com/epicsystem/MainActivity.kt
+++ EpicMobileApp/android/app/src/main/java/com/epicsystem/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.epicmobileapp
+package com.epicsystem
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
--- EpicMobileApp/android/app/src/main/java/com/epicsystem/MainApplication.kt
+++ EpicMobileApp/android/app/src/main/java/com/epicsystem/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.epicmobileapp
+package com.epicsystem
 
 import android.app.Application
 import com.facebook.react.PackageList
