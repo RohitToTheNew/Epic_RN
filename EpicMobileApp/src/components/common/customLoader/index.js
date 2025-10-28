aba2afd01bd08e77962ad85d5cc2f086051a0738
Vishal.Madheshiya <vishal.madheshiya@audioenhancement.com>
2025-10-28 19:03:14 +0530

    udpated

--- EpicMobileApp/src/components/common/customLoader/index.js
+++ EpicMobileApp/src/components/common/customLoader/index.js
@@ -1,28 +1,29 @@
 import React from 'react';
-import {View, Modal} from 'react-native';
+import { View, Modal } from 'react-native';
 import styles from './styles';
 import LottieView from 'lottie-react-native';
 import { useSelector } from 'react-redux';
 
 const CustomLoader = (loaderChangeStyle) => {
-  const {isLoading} = useSelector(state => state.globalReducer)
-  return(
-  <Modal
-    supportedOrientations={['portrait']}
-    transparent
-    visible={!!isLoading ? isLoading : false}
-    onRequestClose={() => {}}>
-    <View style={styles.container}>
-      <View style={[styles.loaderViewStyle, loaderChangeStyle]}>
-      <LottieView
-        source={require('../../../assets/lottieJson/coloredLoader.json')}
-        autoPlay
-        loop={true}
-        speed={0.8}
-      />
+  const { isLoading } = useSelector(state => state.globalReducer)
+  return (
+    <Modal
+      supportedOrientations={['portrait']}
+      transparent
+      visible={!!isLoading ? isLoading : false}
+      onRequestClose={() => { }}>
+      <View style={styles.container}>
+        <View style={[styles.loaderViewStyle, loaderChangeStyle]}>
+          <LottieView
+            source={require('../../../assets/lottieJson/coloredLoader.json')}
+            autoPlay
+            loop={true}
+            speed={0.8}
+            style={{ width: '100%', height: '100%' }}
+          />
+        </View>
       </View>
-    </View>
-  </Modal>
+    </Modal>
   )
 };
 
--- EpicMobileApp/src/components/common/pagingStatusView/sendingPageAnimation.js
+++ EpicMobileApp/src/components/common/pagingStatusView/sendingPageAnimation.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import { View, Text } from 'react-native';
 import styles from './styles';
-import {translate} from '../../../translations/translationHelper';
+import { translate } from '../../../translations/translationHelper';
 import utils from '../../../utils';
 import LottieView from 'lottie-react-native';
 
-const {tablet} = utils;
-const SendingPageAnimation = ({}) => {
+const { tablet } = utils;
+const SendingPageAnimation = ({ }) => {
   const renderSendingAnimation = () => {
     return (
       <View>
@@ -15,6 +15,7 @@ const SendingPageAnimation = ({}) => {
           autoPlay
           loop={true}
           resizeMode="cover"
+          style={{ width: '100%', height: '100%' }}
         />
         <Text style={styles.textStyle}>{translate('sendingMeassage')}</Text>
       </View>
@@ -29,7 +30,7 @@ const SendingPageAnimation = ({}) => {
           autoPlay
           loop={true}
           resizeMode="cover"
-          style = {styles.lottieStyle}
+          style={styles.lottieStyle}
         />
         <Text style={styles.textStyle1}>{translate('sendingMeassage')}</Text>
       </View>
